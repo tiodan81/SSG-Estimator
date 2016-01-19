4e0258ff470d2b78b0857c98374db25a56cd263e
Dan Schwartz <dpschwartz@gmail.com>
2016-01-18 21:37:13 -0800

    Add user create and login forms, begin writing authentication and login functions.

--- js/user.js
+++ js/user.js
@@ -2,6 +2,8 @@ var firebase = new Firebase('https://ssgestimator.firebaseio.com/');
 var projects = firebase.child('projects');
 var users = firebase.child('users');
 
+
+var user = {};
 // {
 //   'projects': {
 //     'one': {
@@ -14,8 +16,7 @@ var users = firebase.child('users');
 //         'mulchZones': [{}, {}],
 //         'volume': 10,
 //         'price': 20
-//       },
-//
+//       }
 //     }
 //   },
 //   'users': {
@@ -39,7 +40,7 @@ firebase.set({    //set JSON formatted object to FB; overwrites node and all chi
   }
 });
 
-firebase.update() //update some keys at node
+firebase.update(); //update some keys at node
 
 firebase.child('key').on('value', function(snapshot) {    //read. 'value' event fires once for initial state of data, also every time data changes
   console.log(snapshot.val());                            //gets a snapshot including all child data
@@ -47,13 +48,45 @@ firebase.child('key').on('value', function(snapshot) {    //read. 'value' event 
   console.log('Epic fail: ' + errorObject.code);
 });
 
-firebase.createUser({
-  email     : 'user@user.com',
-  password  : 'testing'
-}, function(error, userData) {
-  if (error) {
-    console.log('error creating user: ', error);
-  } else {
-    console.log('Successfully created user account with uid: ', userData.uid);
-  }
+user.create = function(event) {
+  event.preventDefault();
+  user.email = $('#new-user').val();
+  var userPassword = $('#new-password').val();
+  firebase.createUser({
+    email     : user.email,
+    password  : userPassword
+  }, function(error, userData) {
+    if (error) {
+      alert(error);
+      console.log('error creating user: ', error);
+    } else {
+      console.log('Successfully created user account with uid: ', userData.uid);
+      user.authenticate(userPassword);
+    }
+  });
+};
+
+user.authLogin = function() {
+
+};
+
+user.authenticate = function(pwd) {
+  firebase.authWithPassword({
+    email     : user.email,
+    password  : pwd
+  }, function(error, authData) {
+    if (error) {
+      console.log('Login failed', error);
+      alert('Login failed. Please try again or create an account.');
+    } else {
+      console.log('Authenticated successfully with payload: ', authData);
+      //do we need uid here? do we get one?
+      //load user's projects
+    }
+  });
+};
+
+$(function() {
+  $('#new-user-form').submit(user.create);
+  $('#login-form').submit(user.authenticate);
 });
