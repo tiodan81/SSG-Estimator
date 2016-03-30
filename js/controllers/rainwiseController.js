6763c991e761c2719085555fbc98fbb5272e1ec6
Dan Schwartz <dpschwartz@gmail.com>
2016-03-30 13:42:53 -0700

    Render rainwise table.

--- js/controllers/rainwiseController.js
+++ js/controllers/rainwiseController.js
@@ -6,7 +6,7 @@ rainwiseController.init = function() {
 
 rainwiseController.save = function() {
   let newRW = rainwise.build()
-  console.log(newRW);
   rainwise.calcs(newRW)
   rainwise.saveToProject(newRW)
+  rainwiseView.render(newRW)
 }
--- js/views/rainwiseView.js
+++ js/views/rainwiseView.js
@@ -10,7 +10,6 @@ rainwiseView.handleSave = function() {
   $('#rainwiseForm').off('submit').on('submit', function(e) {
     e.preventDefault()
     let $val = $('#rainwise-save').val()
-    console.log($val);
     if ($val === 'save') {
       rainwiseController.save()
       rainwiseView.clearForm()
@@ -18,6 +17,27 @@ rainwiseView.handleSave = function() {
   })
 }
 
-rainwiseView.clearForm = function() {
+rainwiseView.render = function(rw) {
+  $('#rainwise-table').html(rainwiseView.makeTable(rw))
+  if ($('#rainwise-display').css('display') === 'none') {
+    $('#rainwise-display').show()
+  }
+  //edit/delete buttons?
+}
 
+rainwiseView.makeTable = function(rw) {
+  let table = `<tr><th>Item</th><th>Amount</th><th>Cost</th><th>Tax</th><th>Total</th></tr>`
+  if (rw.gutterLength) {
+    table += `<tr><td>Gutter</td><td>${rw.gutterLength} ft</td><td>$${rw.gutterCost}</td><td>$${rw.gutterTax}</td><td>$${rw.gutterTotal}</td></tr>`
+  }
+  if (rw.inspection) {
+    table += `<tr><td>Inspection</td><td></td><td>$${rw.inspectionCost}</td><td></td><td>$${rw.inspectionCost}</td></tr>`
+  }
+  table += `<tr class="total-row"><td>Total</td><td></td><td>$${rw.subtotal}</td><td>$${rw.gutterTax}</td><td>$${rw.total}</td></tr>`
+  return table
+}
+
+rainwiseView.clearForm = function() {
+  $('#gutterFt').val('')
+  $('#rw-inspection').prop('checked', false)
 }
